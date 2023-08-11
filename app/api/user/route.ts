c2291edf032982e7c4ee8a38b559323def3daa9f
ooopiz <r.huang850@gmail.com>
2023-08-11 16:16:16 +0800

    Add crypto

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,3 +1,5 @@
+import sha256 from 'crypto-js/sha256';
+import logger from "@/utils/logger";
 import prisma from "@/utils/prisma";
 import { NextResponse } from "next/server"
 
@@ -10,9 +12,15 @@ export async function POST(request: Request) {
   }
 
   const user = await prisma.user.findUnique({where: { name: username }})
-  if (user) {
+  if (!user) {
+    logger.info(`User no found: ${username}`)
+    return NextResponse.json({}, { status: 401 });
+  }
+  if (sha256(password).toString() === user.password) {
+    logger.info(`User login: ${username}`)
     return NextResponse.json(user);
   } else {
+    logger.info(`User password mismatch: ${username}`)
     return NextResponse.json({}, { status: 401 });
   }
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,15 @@
 import { PrismaClient } from '@prisma/client'
+import sha256 from 'crypto-js/sha256';
 
 const prisma = new PrismaClient()
 
 async function main() {
+  const passwordHash = sha256('pass123').toString();
+
   const user = await prisma.user.upsert({
     where: { name: 'admin' },
-    update: { password: 'password'},
-    create: { name: 'admin', password: 'password' },
+    update: { password: passwordHash },
+    create: { name: 'admin', password: passwordHash },
   })
   console.log(user)
 }
