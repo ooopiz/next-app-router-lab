73f076b74c0999decc02f77d06ae838a4395e5d0
ooopiz <r.huang850@gmail.com>
2023-08-12 15:28:10 +0800

    Fix css

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { getServerSession } from "next-auth"
 
+export const metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   const session = await getServerSession(authOptions)
   return <>
