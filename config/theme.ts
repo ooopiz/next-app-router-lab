8b1464d0e43fc08eb83677afa5c7efcc1f827dbc
ooopiz <r.huang850@gmail.com>
2023-08-12 15:02:04 +0800

    add antd layout

---
+++ config/theme.ts
@@ -0,0 +1,12 @@
+import type { ThemeConfig } from 'antd';
+import { theme as atheme } from 'antd';
+
+const theme: ThemeConfig = {
+  algorithm: atheme.defaultAlgorithm
+  //token: {
+  //  fontSize: 16,
+  //  colorPrimary: '#52c41a',
+  //},
+};
+
+export default theme;
